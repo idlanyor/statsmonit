182cc11ff398d2a6a9c386bbf78b8084743d943d
idlanyor <prasbhara0604@gmail.com>
2025-11-29 13:43:43 +0700

    ganti port default

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { getStats } from './lib/stats.js'
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8088
 
 // Create Next.js app
 const app = next({ dev, hostname, port })
